31c23042094c4b3dd5f1807eab571cc2e6fbbb23
David Lourenço <david.lan.lou@gmail.com>
2024-10-11 14:07:44 +0100

    Create mock url protocol.

--- CensusFlowTests/UnitTests/Mocks/MockURLProtocol.swift
+++ CensusFlowTests/UnitTests/Mocks/MockURLProtocol.swift
@@ -7,3 +7,37 @@
 
 import Foundation
 @testable import CensusFlow
+
+class MockURLProtocol: URLProtocol {
+    
+    static var stubResponseData: Data?
+    static var error: Error?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        
+        if let error = MockURLProtocol.error{
+            
+            let stubError = NSError(domain: "StubNSError", code: -99, userInfo: [NSLocalizedDescriptionKey: error.localizedDescription])
+            
+            self.client?.urlProtocol(self, didFailWithError: stubError)
+            
+        }else{
+            
+            self.client?.urlProtocol(self, didLoad: MockURLProtocol.stubResponseData ?? Data())
+            
+        }
+        
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {}
+}
