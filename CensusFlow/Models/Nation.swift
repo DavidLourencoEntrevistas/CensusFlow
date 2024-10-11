eeb2e191d0f40b645c048564b97dc89d1f33ec39
David Lourenço <david.lan.lou@gmail.com>
2024-10-11 12:05:20 +0100

    Create models.

--- CensusFlow/Models/Nation.swift
+++ CensusFlow/Models/Nation.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Nation : Decodable {
+    
+    let idNation : String
+    let nation : String
+    let idYear : Int
+    let year : String
+    let population: Int
+    let slugNation: String
+    
+    enum CodingKeys: String, CodingKey{
+        case idNation = "ID Nation"
+        case nation = "Nation"
+        case idYear = "ID Year"
+        case year = "Year"
+        case population = "Population"
+        case slugNation = "Slug Nation"
+    }
+
+}
--- CensusFlow/Models/State.swift
+++ CensusFlow/Models/State.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct State : Decodable {
+    
+    let idState : String
+    let state : String
+    let idYear : Int
+    let year : String
+    let population: Int
+    let slugState: String
+    
+    enum CodingKeys: String, CodingKey{
+        case idState = "ID State"
+        case state = "State"
+        case idYear = "ID Year"
+        case year = "Year"
+        case population = "Population"
+        case slugState = "Slug State"
+    }
+    
+    
+}
