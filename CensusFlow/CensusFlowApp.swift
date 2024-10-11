0bc62011111bfaf1708247f63686c5ecfccd5e54
David Lourenço <david.lan.lou@gmail.com>
2024-10-11 11:40:37 +0100

    Initial Commit

---
+++ CensusFlow/CensusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CensusFlowApp.swift
+//  CensusFlow
+//
+//  Created by David Lourenço on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CensusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
